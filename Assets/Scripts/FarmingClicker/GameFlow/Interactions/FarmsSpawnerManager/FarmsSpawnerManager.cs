559702135ce88d2724e5e6cd190c60256ae24031
MaciejJasielewicz <m.jasielewicz@miamienglish.io>
2023-01-19 13:12:53 +0100

    Added Farming Spawner Manager

--- Assets/Scripts/FarmingClicker/GameFlow/Interactions/FarmsSpawnerManager/FarmsSpawnerManager.cs
+++ Assets/Scripts/FarmingClicker/GameFlow/Interactions/FarmsSpawnerManager/FarmsSpawnerManager.cs
@@ -4,29 +4,172 @@ using Core.Message;
 using Core.Message.Interfaces;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace FarmingClicker.GameFlow.Interactions.FarmsSpawnerManager
 {
     public class FarmsSpawnerManager : SerializedMonoBehaviour, IMessageReceiver
     {
-        [SerializeField, InlineEditor] private GameObject farmGameObject;
-        [SerializeField, InlineEditor] private Vector3 positionOfFirstFarm;
-
-        private int currentFarmsCount = 0;
+        
+        [SerializeField, InlineEditor] private GameObject granaryBuilding;
+        [SerializeField, InlineEditor] private GameObject shoppingBuilding;
+        
+        [SerializeField, InlineEditor] private GameObject farmPathGameObject;
+        [SerializeField, InlineEditor] private GameObject farmPathFillerGameObject;
+        
+        [SerializeField, InlineEditor] private GameObject farmFieldGameObject;
+        [SerializeField, InlineEditor] private GameObject farmFieldFillerGameObject;
+        
+        private Vector3 positionOfGranaryBuilding;
+        private Vector3 positionOfShoppingBuilding;
+        
+        private Vector3 positionOfFirstFarmPath;
+        private Vector3 positionOfFirstFarmPathFiller;
+        private Vector3 positionOfFirstFarmFieldFiller;
+        private Vector3 positionOfFirstFarmField;
+        
+        private float xPosOfLeftEdgeOfScreen = 0;
+        private SpriteRenderer spriteRendererOfFarmPathGameObject;
+        private SpriteRenderer spriteRendererFillerGameObject;
+        
+        private Vector3 leftEdgePosition;
+        private Vector3 rightEdgePosition;
         
         public void Awake()
         {
             Debug.Log("Registering Navigation Manager.");
 
             MessageDispatcher.Instance.RegisterReceiver(this);
+
+            InitializeVariables();
+            GenerateBuildings();
+            GenerateNumberOfFarms(5);
         }
 
-        private void GenerateFarmGameObject()
+        private void InitializeVariables()
         {
-            currentFarmsCount++;
+            spriteRendererOfFarmPathGameObject = farmPathGameObject.GetComponent<SpriteRenderer>();
+            spriteRendererFillerGameObject = farmPathFillerGameObject.GetComponent<SpriteRenderer>();
+            leftEdgePosition = CalculateLeftEdgeOfScreen();
+            rightEdgePosition = CalculateRightEdgeOfScreen();
+            
+            positionOfFirstFarmPath = CalculateFinalPosForTheFirstFarmPath();
+            positionOfFirstFarmPathFiller = CalculateFinalPosForTheFirstFarmPathFiller();
+            positionOfFirstFarmField = CalculateFinalPosForTheFirstFarmField();
+            positionOfFirstFarmFieldFiller = CalculateFinalPosForTheFirstFarmFieldFiller();
+
+            positionOfGranaryBuilding = CalculateFinalPosForTheGranaryBuilding();
+            positionOfShoppingBuilding = CalculateFinalPosForTheShoppingBuilding();
+        }
+
+        private void GenerateBuildings()
+        {
+            Instantiate(granaryBuilding, positionOfGranaryBuilding, Quaternion.identity);
+            Instantiate(shoppingBuilding, positionOfShoppingBuilding, Quaternion.identity);
+        }
+        
+        private void GenerateNumberOfFarms(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                GenerateFarmGameObject(i);
+            }
+        }
+        
+        
+        
+        private Vector3 CalculateLeftEdgeOfScreen()
+        {
+            Vector3 leftEdgePos = Camera.main.ViewportToWorldPoint(new Vector3(0, 0.5f, Camera.main.nearClipPlane));
+            return leftEdgePos;
+        }
+        
+        private Vector3 CalculateRightEdgeOfScreen()
+        {
+            Vector3 rightEdgePos = Camera.main.ViewportToWorldPoint(new Vector3(1, 0.5f, Camera.main.nearClipPlane));
+            return rightEdgePos;
+        }
+
+        private Vector3 CalculateFinalPosForTheGranaryBuilding()
+        {
+            var spriteRendererGranaryBuilding = granaryBuilding.GetComponent<SpriteRenderer>();
+
+            float posXGranaryBuilding = leftEdgePosition.x + (spriteRendererGranaryBuilding.bounds.size.x)/2;
+            float posYGranaryBuilding = leftEdgePosition.y + (spriteRendererGranaryBuilding.bounds.size.y)/2;
+            return new Vector3(posXGranaryBuilding,posYGranaryBuilding,0);
+        }
+
+        private Vector3 CalculateFinalPosForTheShoppingBuilding()
+        {
+            var spriteRendererShoppingBuilding = shoppingBuilding.GetComponent<SpriteRenderer>();
+
+            float posXGranaryBuilding = rightEdgePosition.x - (spriteRendererShoppingBuilding.bounds.size.x)/2;
+            float posYGranaryBuilding = rightEdgePosition.y + (spriteRendererShoppingBuilding.bounds.size.y)/2;
+            return new Vector3(posXGranaryBuilding,posYGranaryBuilding,0);
+        }
+        
+        
+        private Vector3 CalculateFinalPosForTheFirstFarmPath()
+        {
+            float posXLeftEdgeWithHalfOfSpriteW = leftEdgePosition.x + (spriteRendererOfFarmPathGameObject.bounds.size.x)/2;
+            float middleOfTheScreenY = leftEdgePosition.y;
+            float posYMiddleWithHalfOfSpriteW = middleOfTheScreenY - (spriteRendererOfFarmPathGameObject.bounds.size.y/2);
+            return new Vector3(posXLeftEdgeWithHalfOfSpriteW,posYMiddleWithHalfOfSpriteW,0);
+        }
+        
+        private Vector3 CalculateFinalPosForTheFirstFarmField()
+        {
+            Vector3 finalPosOfFarmField = positionOfFirstFarmPath;
+            
+            float newX = finalPosOfFarmField.x + spriteRendererOfFarmPathGameObject.bounds.size.x/2 + 
+                         farmFieldGameObject.GetComponent<SpriteRenderer>().bounds.size.x/2;
+            finalPosOfFarmField.x = newX;
+            return finalPosOfFarmField;
+        }
+        
+        private Vector3 CalculateFinalPosForTheFirstFarmPathFiller()
+        {
+            Vector3 finalPosOfFirstFarmPathFiller = positionOfFirstFarmPath;
+            
+            float newY = finalPosOfFirstFarmPathFiller.y - (spriteRendererOfFarmPathGameObject.bounds.size.y/2 + 
+                                                            spriteRendererFillerGameObject.bounds.size.y/2);
+            finalPosOfFirstFarmPathFiller.y = newY;
+            return finalPosOfFirstFarmPathFiller;
+        }
+        
+        private Vector3 CalculateFinalPosForTheFirstFarmFieldFiller()
+        {
+            Vector3 finalPosOfFirstFarmFieldFiller = positionOfFirstFarmField;
+            float newY = finalPosOfFirstFarmFieldFiller.y - (spriteRendererOfFarmPathGameObject.bounds.size.y/2 + 
+                                                             spriteRendererFillerGameObject.bounds.size.y/2);
+            finalPosOfFirstFarmFieldFiller.y = newY;
+            return finalPosOfFirstFarmFieldFiller;
+        }
+
+        
+        private void GenerateFarmGameObject(int nOfFarm)
+        {
+            Debug.Log("GenerateFarmGameObject");            
+            
+            Vector3 positionOfCurrentFarmPath = positionOfFirstFarmPath;
+            positionOfCurrentFarmPath.y -= (spriteRendererOfFarmPathGameObject.bounds.size.y * nOfFarm + spriteRendererFillerGameObject.bounds.size.y * nOfFarm);
+            
+            Vector3 positionOfCurrentFarmField = positionOfFirstFarmField;
+            positionOfCurrentFarmField.y -= (spriteRendererOfFarmPathGameObject.bounds.size.y * nOfFarm + spriteRendererFillerGameObject.bounds.size.y * nOfFarm);
+            
+            Vector3 positionOfCurrentFarmPathFiller = positionOfFirstFarmPathFiller;
+            positionOfCurrentFarmPathFiller.y -= (spriteRendererOfFarmPathGameObject.bounds.size.y * nOfFarm + spriteRendererFillerGameObject.bounds.size.y * nOfFarm);
+
+            Vector3 positionOfCurrentFarmFieldFiller = positionOfFirstFarmFieldFiller;
+            positionOfCurrentFarmFieldFiller.y -= (spriteRendererOfFarmPathGameObject.bounds.size.y * nOfFarm + spriteRendererFillerGameObject.bounds.size.y * nOfFarm);
             
             
+            Instantiate(farmPathGameObject, positionOfCurrentFarmPath, Quaternion.identity);
+            Instantiate(farmFieldGameObject, positionOfCurrentFarmField, Quaternion.identity);
+            Instantiate(farmPathFillerGameObject, positionOfCurrentFarmPathFiller, Quaternion.identity);
+            Instantiate(farmFieldFillerGameObject, positionOfCurrentFarmFieldFiller, Quaternion.identity);
             
+
         }
         
         
